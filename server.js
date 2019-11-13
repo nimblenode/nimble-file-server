a06154374e40620465b9978409eb914524366426
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-11-12 23:17:50 -0500

    Updated HTTP server

--- server.js
+++ server.js
@@ -1,42 +1,17 @@
 'use strict';
 
-// Include hapijs
-const Hapi = require('@hapi/hapi');
+// HTTP Server
+let http = require('http')
 
-// Routes
-const Routes = require("./routes")
+// Parse URL
+let parseUrl = require('parseurl')
 
-// Inert
-const Inert = require('@hapi/inert');
+// Send
+let send = require('send')
 
-// Asynchnous wrapper
-const init = async () => {
+// Server HTTP
+let server = http.createServer(function onRequest (req, res) {
+    send(req, parseUrl(req).pathname, { root: './storage/' }).pipe(res)
+})
 
-    // Initialize server
-    const server = Hapi.server({
-          port: 3000,
-          host: '0.0.0.0'
-    });
-
-    // Add file server
-    await server.register(Inert);
-
-    // Register routes
-    server.route(Routes.default);
-    server.route(Routes.files);
-
-    // Start the server
-    await server.start();
-
-    // Display that server has started
-    console.log('FILE SERVER %s', server.info.uri);
-};
-
-
-// Quit the server if we have any unhandled exceptions
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+server.listen(3000)
